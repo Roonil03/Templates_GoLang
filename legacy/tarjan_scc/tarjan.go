3a18044698bb80cfa4513b102235a07f639c0410
Roonil03 <aryanvivek05@gmail.com>
2026-06-13 17:22:18 +0530

    Tarjan SCC old ver added

---
+++ legacy/tarjan_scc/tarjan.go
@@ -0,0 +1,77 @@
+package tarjan_scc
+
+type TarjanSCC struct {
+	graph [][]int
+	dfn   []int
+	low   []int
+	stack []int
+	inStk []bool
+	timer int
+	sccs  [][]int
+}
+
+func NewTarjanSCC(n int) *TarjanSCC {
+	return &TarjanSCC{
+		graph: make([][]int, n),
+		dfn:   make([]int, n),
+		low:   make([]int, n),
+		inStk: make([]bool, n),
+	}
+}
+
+func (t *TarjanSCC) AddEdge(u, v int) {
+	t.graph[u] = append(t.graph[u], v)
+}
+
+func (t *TarjanSCC) Build() [][]int {
+	n := len(t.graph)
+	for i := 0; i < n; i++ {
+		t.dfn[i] = -1
+		t.low[i] = -1
+	}
+	t.timer = 0
+	t.sccs = nil
+	t.stack = make([]int, 0, n)
+
+	for i := 0; i < n; i++ {
+		if t.dfn[i] == -1 {
+			t.dfs(i)
+		}
+	}
+	return t.sccs
+}
+
+func (t *TarjanSCC) dfs(u int) {
+	t.dfn[u] = t.timer
+	t.low[u] = t.timer
+	t.timer++
+	t.stack = append(t.stack, u)
+	t.inStk[u] = true
+
+	for _, v := range t.graph[u] {
+		if t.dfn[v] == -1 {
+			t.dfs(v)
+			if t.low[v] < t.low[u] {
+				t.low[u] = t.low[v]
+			}
+		} else if t.inStk[v] {
+			if t.dfn[v] < t.low[u] {
+				t.low[u] = t.dfn[v]
+			}
+		}
+	}
+
+	if t.low[u] == t.dfn[u] {
+		var scc []int
+		for {
+			v := t.stack[len(t.stack)-1]
+			t.stack = t.stack[:len(t.stack)-1]
+			t.inStk[v] = false
+			scc = append(scc, v)
+			if u == v {
+				break
+			}
+		}
+		t.sccs = append(t.sccs, scc)
+	}
+}
