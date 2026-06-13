cb66173bdb94b0fdcd1f6a5eeadb1c41b8fe13e9
Roonil03 <aryanvivek05@gmail.com>
2026-06-13 17:23:11 +0530

    Mo's Algorithm code added

---
+++ mos_algorithm/mos.go
@@ -0,0 +1,54 @@
+package mos_algorithm
+
+import "sort"
+
+type Query struct {
+	L, R, Idx int
+}
+
+func MosAlgorithm[T any, R any](
+	arr []T,
+	queries []Query,
+	blockSize int,
+	add func(idx int),
+	remove func(idx int),
+	getAnswer func() R,
+) []R {
+	sort.Slice(queries, func(i, j int) bool {
+		blockI := queries[i].L / blockSize
+		blockJ := queries[j].L / blockSize
+		if blockI != blockJ {
+			return blockI < blockJ
+		}
+		if blockI%2 == 1 {
+			return queries[i].R < queries[j].R
+		}
+		return queries[i].R > queries[j].R
+	})
+
+	answers := make([]R, len(queries))
+	currL := 0
+	currR := -1
+
+	for _, q := range queries {
+		for currL > q.L {
+			currL--
+			add(currL)
+		}
+		for currR < q.R {
+			currR++
+			add(currR)
+		}
+		for currL < q.L {
+			remove(currL)
+			currL++
+		}
+		for currR > q.R {
+			remove(currR)
+			currR--
+		}
+		answers[q.Idx] = getAnswer()
+	}
+
+	return answers
+}
